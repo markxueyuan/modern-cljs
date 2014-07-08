fb92cc69face9112550fe852ea0a97914940525f
markxueyuan <ziyaajiang@gmail.com>
2014-07-08 14:43:25 +0800

    tutorial3 done

--- project.clj
+++ project.clj
@@ -6,12 +6,15 @@
 
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2069"]]
+                 [org.clojure/clojurescript "0.0-2069"]
+                 [compojure "1.1.6"]]
 
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :plugins [[lein-cljsbuild "1.0.0"]
+            [lein-ring "0.8.8"]]
 
   :cljsbuild {:builds
               [{:source-paths ["src/cljs"]
                 :compiler {:output-to "resources/public/js/modern.js"
                            :optimizations :whitespace
-                           :pretty-print true}}]})
+                           :pretty-print true}}]}
+  :ring {:handler modern-cljs.core/handler})
--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -1,6 +1,11 @@
-(ns modern-cljs.core)
+(ns modern-cljs.core
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app-routes
+  (GET "/" [] "<a href='/simple.html'>开启藏密生活</a>")
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def handler (handler/site app-routes))
